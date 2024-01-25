584cc96550c54564d5bfc69b2f1f578ad65b8202
vinjal-jain <vinjaljain14it@gmail.com>
2024-01-25 18:22:39 +0530

    Pushing POST request detail in .json file

--- app.js
+++ app.js
@@ -17,11 +17,11 @@ app.post('/', (req,res) => {
 })
 */
 const tours = JSON.parse(
-fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 
-app.get('/api/v1/tours', (req,res) => {
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -32,9 +32,30 @@ app.get('/api/v1/tours', (req,res) => {
 
 })
 
-app.post('/api/v1/tours', (req,res)=> {
-    console.log(req.body);
-    res.send("Done");
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id +1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+
+    );
+
+
+    
 
 })
 
