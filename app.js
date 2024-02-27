b23189845b2d7f65cab14007748163aebed7fe47
vinjal-jain <vinjaljain14it@gmail.com>
2024-02-27 12:22:49 +0530

    install morgan and adding some comments

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const express = require('express');
 const { get } = require('http');
+const morgan = require('morgan');
 
 
 const app = express();
+
+
+
+// 1 ) MIDDLEWARES 
 app.use(express.json());
 
 app.use((req,res, next) => {
@@ -20,6 +25,8 @@ app.use((req,res, next) => {
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDLERS 
  const getAllTours = (req,res) => {
     console.log(req.requestedTime);
     res.status(200).json({
@@ -114,6 +121,8 @@ app.post('/api/v1/tours', createTour);
 app.patch('/api/v1/tours/:id', updateTour);
 app.delete('/api/v1/tours/:id',deleteTour);
 */
+
+// 3) ROUTES 
 app
     .route('/api/v1/tours')
     .get(getAllTours)
@@ -125,6 +134,7 @@ app
     .patch(updateTour)
     .delete(deleteTour);
 
+// 4) START SERVER
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}....`);
