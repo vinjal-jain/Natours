e3620e90372543f8558f282a6acd8ae3e8596f1a
vinjal-jain <vinjaljain14it@gmail.com>
2024-03-04 22:52:16 +0530

    Chaining Multiple Middleware Functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,7 +6,6 @@ const tours = JSON.parse(
 
 exports.checkID = (req,res,next,val)=> {
     console.log(`Tour id is : ${val}`);
-
 if (req.params.id * 1 > tours.length){
     return res.status(404).json({
         status: 'fail',
@@ -14,6 +13,16 @@ if (req.params.id * 1 > tours.length){
     });
 };
 next();
+}
+
+exports.checkBody = (req,res,next)=> {
+    if(!req.body.name || !req.body.price){
+        return res.status(400).json({
+            status : 'fail',
+            message: 'Missing name or price'
+        })
+    }
+    next();
 }
 
 exports. getAllTours = (req,res) => {
