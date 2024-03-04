a781286135ca6bb3c890b6944e82ac67778249ac
vinjal-jain <vinjaljain14it@gmail.com>
2024-03-04 17:10:53 +0530

    implementing the user routes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const { get } = require('http');
 const morgan = require('morgan');
 
-
 const app = express();
 
 // 1 ) MIDDLEWARES
@@ -114,6 +113,41 @@ const deleteTour = (req,res) => {
     };
 }
 
+const getAllUsers = (req,res) => {
+    res.status(500).json({
+        status : 'error', 
+        message: 'This route is not yet defined!'
+});
+}
+
+const getUser= (req,res) => {
+    res.status(500).json({
+        status : 'error', 
+        message: 'This route is not yet defined!'
+});
+}
+
+const createUser = (req,res) => {
+    res.status(500).json({
+        status : 'error', 
+        message: 'This route is not yet defined!'
+});
+}
+
+const updateUser = (req,res) => {
+    res.status(500).json({
+        status : 'error', 
+        message: 'This route is not yet defined!'
+});
+}
+
+const deleteUser = (req,res) => {
+    res.status(500).json({
+        status : 'error', 
+        message: 'This route is not yet defined!'
+});
+}
+
 /*
 app.get('/api/v1/tours', getAllTours);
 app.get('/api/v1/tours/:id', getTour);
@@ -133,6 +167,17 @@ app
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
+
+app
+    .route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser);
+
+app
+    .route('/api/v1/users/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);
 
 // 4) START SERVER
 const port = 3000;
