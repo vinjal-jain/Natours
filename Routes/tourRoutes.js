c396899774c5bec7a30bdb7af320b2094c346263
vinjal-jain <vinjaljain14it@gmail.com>
2024-03-04 22:29:28 +0530

    Param Middleware

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -3,6 +3,9 @@ const express = require('express');
 const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
+router.param('id', tourController.checkID);
+
+
 router
     .route('/')
     .get(tourController.getAllTours)
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,7 +4,17 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req,res,next,val)=> {
+    console.log(`Tour id is : ${val}`);
 
+if (req.params.id * 1 > tours.length){
+    return res.status(404).json({
+        status: 'fail',
+        message: 'Invalid ID'
+    });
+};
+next();
+}
 
 exports. getAllTours = (req,res) => {
     console.log(req.requestedTime);
@@ -23,13 +33,7 @@ exports.getTour = (req,res)=> {
     const id = req.params.id * 1;
     const tour = tours.find(el => el.id === id);
 
-    //if (id > tours.length) {
-    if (!tour) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
+    
 
     res.status(200).json({
         status: 'success',
@@ -62,33 +66,18 @@ exports.createTour =  (req, res) => {
     );
 }
 
-exports. updateTour =  (req, res) => {
-    // console.log(req.body);
-
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-
-    tours.push(newTour);
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/tours-simple.json`,
-        JSON.stringify(tours),
-        err => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    tour: newTour
-                }
-            });
+exports. updateTour =  (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '< Updated tour here........>'
         }
-    );
+    })
 }
 
 exports. deleteTour = (req,res) => {
-    if (req.params.id * 1 > tours.length){
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    };
+    res.status(200).json({
+        status: 'success',
+        data: null
+    })
 };
